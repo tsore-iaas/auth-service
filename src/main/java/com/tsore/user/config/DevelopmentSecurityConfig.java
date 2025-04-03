4195c1d3f57defcc72de36b20d2db929127bc338
Tchassi Daniel <tchassidaniel@gmail.com>
2025-04-03 05:40:26 +0100

    update cors config

--- src/main/java/com/tsore/user/config/DevelopmentSecurityConfig.java
+++ src/main/java/com/tsore/user/config/DevelopmentSecurityConfig.java
@@ -5,29 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Profile("development")
-public class DevelopmentSecurityConfig implements WebMvcConfigurer {
+public class DevelopmentSecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers(new AntPathRequestMatcher("/**"));
     }
-
-    // Méthode pour autoriser CORS
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // Permettre CORS pour toutes les origines
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Autoriser toutes les origines
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Méthodes HTTP autorisées
-                .allowedHeaders("*") // Autoriser tous les en-têtes
-                .allowCredentials(false); // Vous pouvez ajuster cela selon vos besoins (faux si vous ne voulez pas de
-                                          // cookies ou de données d'authentification)
-    }
-
 }
---
+++ src/main/java/com/tsore/user/config/corsConfig.java
@@ -0,0 +1,25 @@
+package com.tsore.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+            }
+
+            public void addCorsMapping(CorsRegistry registry) {
+                registry.addMapping("/**");
+
+            }
+        };
+    }
+}
